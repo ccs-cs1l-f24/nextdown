330b2e8c9f9c4a1e8cbd384d77dd921f424f5e69
Edwin Chang <edwinchang2024@gmail.com>
2024-11-10 16:50:23 -0800

    put events into UI layout

---
+++ src/commonMain/kotlin/ui/EventCard.kt
@@ -0,0 +1,82 @@
+package ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import model.Event
+import theme.JetBrainsMono
+
+@Composable
+fun EventCard(event: Event, currentTimeMs: Long, emphasisState: Float = 0f, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(16.dp)) {
+        if (emphasisState < 0.6) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .then(if (emphasisState == 0f) Modifier else Modifier.fillMaxHeight())
+                        .padding(24.dp),
+            ) {
+                Column {
+                    Text(event.summary ?: "[unnamed]", style = MaterialTheme.typography.titleMedium)
+                    event.description?.let { Text(it, style = MaterialTheme.typography.bodyMedium) }
+                }
+                Countdown(event.dtStart.toInstant(TimeZone.currentSystemDefault()), currentTimeMs)
+            }
+        } else {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize().padding(24.dp),
+            ) {
+                Text(event.summary ?: "[unnamed]", style = MaterialTheme.typography.titleLarge)
+                Countdown(event.dtStart.toInstant(TimeZone.currentSystemDefault()), currentTimeMs, fontSize = 48.sp)
+                event.description?.let { Text(it, style = MaterialTheme.typography.bodyLarge) }
+            }
+        }
+    }
+}
+
+@Composable
+fun Countdown(time: Instant, currentTimeMs: Long, fontSize: TextUnit = 36.sp, modifier: Modifier = Modifier) {
+    val currentTime = Instant.fromEpochMilliseconds(currentTimeMs)
+    Text(
+        (if (time > currentTime) time - currentTime else currentTime - time).toComponents {
+            days,
+            hours,
+            minutes,
+            seconds,
+            _ ->
+            buildString {
+                if (time < currentTime) append("-")
+                append(days.toString().padStart(2, '0'))
+                append(':')
+                append(hours.toString().padStart(2, '0'))
+                append(':')
+                append(minutes.toString().padStart(2, '0'))
+                append(':')
+                append(seconds.toString().padStart(2, '0'))
+            }
+        },
+        fontSize = fontSize,
+        fontFamily = JetBrainsMono,
+        modifier = modifier,
+    )
+}
--- src/commonMain/kotlin/ui/Home.kt
+++ src/commonMain/kotlin/ui/Home.kt
@@ -3,10 +3,8 @@ package ui
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,10 +20,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.dp
+import kotlin.math.abs
 import kotlin.math.roundToInt
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.delay
 import kotlinx.datetime.Clock
@@ -39,137 +38,158 @@ import model.Event
 @Composable
 fun HomePage() {
     Surface(modifier = Modifier.fillMaxSize()) {
-        val appOpened = remember { Clock.System.now() }
-        var currentTimeMs by rememberSaveable { mutableLongStateOf(Clock.System.now().toEpochMilliseconds()) }
-        LaunchedEffect(true) {
-            currentTimeMs = Clock.System.now().toEpochMilliseconds()
-            while (true) {
-                delay(1000 - (currentTimeMs % 1000))
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            val appOpened = remember { Clock.System.now() }
+            var currentTimeMs by rememberSaveable { mutableLongStateOf(Clock.System.now().toEpochMilliseconds()) }
+            LaunchedEffect(true) {
                 currentTimeMs = Clock.System.now().toEpochMilliseconds()
+                while (true) {
+                    delay(1000 - (currentTimeMs % 1000))
+                    currentTimeMs = Clock.System.now().toEpochMilliseconds()
+                }
             }
-        }
-        fun ldt(instant: Instant) = instant.toLocalDateTime(TimeZone.currentSystemDefault())
-        var calendar by remember {
-            mutableStateOf(
-                Calendar(
-                    listOf(
-                        Event(
-                            uid = "test2",
-                            dtStart = ldt(appOpened + 3.seconds),
-                            dtEnd = ldt(appOpened + 3.hours),
-                            summary = "Test event 2",
-                            description = "This is a test event! How exciting.",
-                        ),
-                        Event(
-                            uid = "test1",
-                            dtStart = ldt(appOpened - 2.days),
-                            dtEnd = ldt(appOpened - 1.days),
-                            summary = "Test event 1",
-                            description = "This is a test event! How exciting.",
-                        ),
-                        Event(
-                            uid = "test3",
-                            dtStart = ldt(appOpened + 1.days),
-                            dtEnd = ldt(appOpened + 2.days),
-                            summary = "Test event 3",
-                            description = "This is a test event! How exciting.",
-                        ),
-                        Event(
-                            uid = "test4",
-                            dtStart = ldt(appOpened + 2.days),
-                            dtEnd = ldt(appOpened + 3.days),
-                            summary = "Test event 4",
-                            description = "This is a test event! How exciting.",
-                        ),
+            fun ldt(instant: Instant) = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+            var calendar by remember {
+                mutableStateOf(
+                    Calendar(
+                        buildList {
+                            add(
+                                Event(
+                                    uid = "test2",
+                                    dtStart = ldt(appOpened + 3.seconds),
+                                    dtEnd = ldt(appOpened + 3.hours),
+                                    summary = "Test event 2",
+                                    description = "This is a test event! How exciting.",
+                                )
+                            )
+                            add(
+                                Event(
+                                    uid = "test1",
+                                    dtStart = ldt(appOpened - 2.days),
+                                    dtEnd = ldt(appOpened - 1.days),
+                                    summary = "Test event 1",
+                                    description = "This is a test event! How exciting.",
+                                )
+                            )
+                            add(
+                                Event(
+                                    uid = "test3",
+                                    dtStart = ldt(appOpened + 1.days),
+                                    dtEnd = ldt(appOpened + 2.days),
+                                    summary = "Test event 3",
+                                    description = "This is a test event! How exciting.",
+                                )
+                            )
+                            add(
+                                Event(
+                                    uid = "test4",
+                                    dtStart = ldt(appOpened + 2.days),
+                                    dtEnd = ldt(appOpened + 3.days),
+                                    summary = "Test event 4",
+                                    description = "This is a test event! How exciting.",
+                                )
+                            )
+                            repeat(10) {
+                                add(
+                                    Event(
+                                        uid = "before$it",
+                                        dtStart = ldt(appOpened - it.minutes * 1000),
+                                        dtEnd = ldt(appOpened),
+                                        summary = "Before $it",
+                                        description = "This event happened already!",
+                                    )
+                                )
+                                add(
+                                    Event(
+                                        uid = "after$it",
+                                        dtStart = ldt(appOpened + it.minutes * 500),
+                                        dtEnd = ldt(appOpened + it.minutes * 600),
+                                        summary = "After $it",
+                                        description = "This event is upcoming!",
+                                    )
+                                )
+                            }
+                        }
                     )
                 )
-            )
-        }
-        val events by derivedStateOf {
-            calendar.events
-                .sortedBy { it.dtStart }
-                .partition {
-                    it.dtStart.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds() < currentTimeMs
-                }
-                .let { (before, after) -> before to after.drop(1) to after.firstOrNull() }
-        }
-        var scroll by remember { mutableFloatStateOf(0f) }
-        var range by remember { mutableStateOf(0..0) }
-        val nextEvent =
-            @Composable {
-                Card(modifier = Modifier.padding(8.dp)) { Text("base element", modifier = Modifier.padding(24.dp)) }
             }
-        val eventsBefore =
-            @Composable {
-                repeat(5) {
-                    OutlinedCard(modifier = Modifier.padding(8.dp)) {
-                        Text("before: element $it", modifier = Modifier.padding(24.dp))
+            val events by derivedStateOf {
+                calendar.events
+                    .sortedBy { it.dtStart }
+                    .partition {
+                        it.dtStart.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds() < currentTimeMs
                     }
-                }
+                    .let { (before, after) -> before to after.drop(1) to after.firstOrNull() }
             }
-        val eventsAfter =
-            @Composable {
-                repeat(10) {
-                    OutlinedCard(modifier = Modifier.padding(8.dp)) {
-                        Text("after: element $it", modifier = Modifier.padding(24.dp))
-                    }
+            var scroll by remember { mutableFloatStateOf(0f) }
+            var range by remember { mutableStateOf(0..0) }
+            val nextEvent =
+                @Composable {
+                    events.second?.let { EventCard(it, currentTimeMs, 1 - abs(scroll / constraints.maxHeight)) }
+                        ?: Text("No upcoming events")
                 }
-            }
-        SubcomposeLayout(
-            modifier =
-                Modifier.fillMaxSize()
-                    .scrollable(
-                        rememberScrollableState { delta ->
-                            val old = scroll
-                            scroll = (scroll + delta).coerceIn(range.first.toFloat()..range.last.toFloat())
-                            scroll - old
-                        },
-                        orientation = Orientation.Vertical,
-                    )
-        ) { constraints ->
-            var slotId = 0
-            val nextEventHeight = subcompose(++slotId) { nextEvent() }.first().measure(Constraints()).height
-            val nextEventFinalHeight =
-                (scroll / constraints.maxHeight *
-                        (0.5f * constraints.maxHeight - nextEventHeight) *
-                        (if (scroll <= 0) 1 else -1) + 0.5f * constraints.maxHeight)
-                    .roundToInt()
-                    .coerceAtLeast(0)
-            val nextEventPlaceable =
-                subcompose(++slotId) { nextEvent() }
-                    .first()
-                    .measure(
-                        Constraints(
-                            minWidth = constraints.maxWidth,
-                            maxWidth = constraints.maxWidth,
-                            minHeight = nextEventFinalHeight,
-                            maxHeight = nextEventFinalHeight,
+            val eventsBefore = @Composable { events.first.first.forEach { EventCard(it, currentTimeMs, 0f) } }
+            val eventsAfter = @Composable { events.first.second.forEach { EventCard(it, currentTimeMs, 0f) } }
+            SubcomposeLayout(
+                modifier =
+                    Modifier.fillMaxSize()
+                        .scrollable(
+                            rememberScrollableState { delta ->
+                                val old = scroll
+                                scroll = (scroll + delta).coerceIn(range.first.toFloat()..range.last.toFloat())
+                                scroll - old
+                            },
+                            orientation = Orientation.Vertical,
                         )
+            ) { constraints ->
+                var slotId = 0
+                val nextEventHeight = subcompose(++slotId) { nextEvent() }.first().measure(Constraints()).height
+                val nextEventFinalHeight =
+                    (scroll / constraints.maxHeight *
+                            (0.5f * constraints.maxHeight - nextEventHeight) *
+                            (if (scroll <= 0) 1 else -1) + 0.5f * constraints.maxHeight)
+                        .roundToInt()
+                        .coerceAtLeast(0)
+                val nextEventPlaceable =
+                    subcompose(++slotId) { nextEvent() }
+                        .first()
+                        .measure(
+                            Constraints(
+                                minWidth = constraints.maxWidth,
+                                maxWidth = constraints.maxWidth,
+                                minHeight = nextEventFinalHeight,
+                                maxHeight = nextEventFinalHeight,
+                            )
+                        )
+                val eventsBeforePlaceables =
+                    subcompose(++slotId) { eventsBefore() }
+                        .map {
+                            it.measure(Constraints(minWidth = constraints.maxWidth, maxWidth = constraints.maxWidth))
+                        }
+                val eventsAfterPlaceables =
+                    subcompose(++slotId) { eventsAfter() }
+                        .map {
+                            it.measure(Constraints(minWidth = constraints.maxWidth, maxWidth = constraints.maxWidth))
+                        }
+                range = -eventsAfterPlaceables.sumOf { it.height }..eventsBeforePlaceables.sumOf { it.height }
+                layout(width = constraints.maxWidth, height = constraints.maxHeight) {
+                    nextEventPlaceable.place(
+                        0,
+                        ((if (scroll <= 0)
+                                scroll / constraints.maxHeight * (0.25f * constraints.maxHeight + nextEventHeight)
+                            else scroll * 0.75f) + 0.25f * constraints.maxHeight)
+                            .roundToInt(),
                     )
-            val eventsBeforePlaceables =
-                subcompose(++slotId) { eventsBefore() }
-                    .map { it.measure(Constraints(minWidth = constraints.maxWidth, maxWidth = constraints.maxWidth)) }
-            val eventsAfterPlaceables =
-                subcompose(++slotId) { eventsAfter() }
-                    .map { it.measure(Constraints(minWidth = constraints.maxWidth, maxWidth = constraints.maxWidth)) }
-            range = -eventsAfterPlaceables.sumOf { it.height }..eventsBeforePlaceables.sumOf { it.height }
-            layout(width = constraints.maxWidth, height = constraints.maxHeight) {
-                nextEventPlaceable.place(
-                    0,
-                    ((if (scroll <= 0)
-                            scroll / constraints.maxHeight * (0.25f * constraints.maxHeight + nextEventHeight)
-                        else scroll * 0.75f) + 0.25f * constraints.maxHeight)
-                        .roundToInt(),
-                )
-                var y = scroll.roundToInt()
-                eventsBeforePlaceables.asReversed().forEach {
-                    y -= it.height
-                    it.place(0, y)
-                }
-                y = constraints.maxHeight + scroll.roundToInt()
-                eventsAfterPlaceables.forEach {
-                    it.place(0, y)
-                    y += it.height
+                    var y = scroll.roundToInt()
+                    eventsBeforePlaceables.asReversed().forEach {
+                        y -= it.height
+                        it.place(0, y)
+                    }
+                    y = constraints.maxHeight + scroll.roundToInt()
+                    eventsAfterPlaceables.forEach {
+                        it.place(0, y)
+                        y += it.height
+                    }
                 }
             }
         }
